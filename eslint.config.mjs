120ce9bfe39d115f9d7dfcb01669c09ce48463ab
ahdkabeerhadi <kabeer@mirrorfolio.com>
2026-04-09 08:29:48 +0530

    chore: configure linting rules, update build scripts, add environment template, and refine gitignore patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,14 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/no-unescaped-entities": "warn",
+      "@next/next/no-img-element": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
